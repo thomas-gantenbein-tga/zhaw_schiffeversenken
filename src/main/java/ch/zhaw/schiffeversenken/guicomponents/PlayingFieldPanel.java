d4ea569f7c022b8da4b4f498c34b0c654c5cf1b4
Thomas Gantenbein <tgantenbein@sunrise.ch>
2017-12-11 17:23:00 +0100

    Creating lines for playing field and first symbols for ships

---
+++ src/main/java/ch/zhaw/schiffeversenken/guicomponents/PlayingFieldPanel.java
@@ -0,0 +1,48 @@
+package ch.zhaw.schiffeversenken.guicomponents;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class PlayingFieldPanel extends JPanel{
+	
+	private List<Shape> shapes = new ArrayList<Shape>();
+	private int playingFieldSize;
+	
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
+		Graphics2D g2 = (Graphics2D) g;
+	    g2.setStroke(new BasicStroke(2));
+		
+		
+		if(this.getWidth()>this.getHeight()) {
+			playingFieldSize = this.getHeight();
+		} else {
+			playingFieldSize = this.getWidth();
+		}
+		
+		
+		for(Shape shape : shapes) {
+			shape.draw(playingFieldSize, g2);
+		}
+	}
+	
+	public void addShape(Shape shape) {
+		shapes.add(shape);
+	}
+
+	public int getSquareSize() {
+		return playingFieldSize;
+	}
+	
+	
+	
+}

8e0e34fbd77a809a7e677c02d7b07a3f92952792
Thomas Gantenbein <tgantenbein@sunrise.ch>
2017-12-14 17:20:04 +0100

    Added javadoc comment.

--- src/main/java/ch/zhaw/schiffeversenken/guicomponents/Shape.java
+++ src/main/java/ch/zhaw/schiffeversenken/guicomponents/Shape.java
@@ -12,7 +12,17 @@ public abstract class Shape {
 	protected Color color;
 	
 	
-	
+	/**
+	 * All parameteres (except color) are given in percent of the the total width of the playing field.
+	 * <p>
+	 * What these percentages mean in absolute terms (pixels) is computed in the "draw" method of each subclass.
+	 * The draw method receives the absolute width/height of the panel on which the playing field is displayed.
+	 * @param centerX
+	 * @param centerY
+	 * @param width
+	 * @param height
+	 * @param color
+	 */
 	public Shape(int centerX, int centerY, double width, double height, Color color) {
 		this.centerX = centerX;
 		this.centerY = centerY;

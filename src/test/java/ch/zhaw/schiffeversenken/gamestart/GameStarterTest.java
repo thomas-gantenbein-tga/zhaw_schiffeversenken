9581e04d4114bab434242fa355759e8ab5033358
Thomas Gantenbein <tgantenbein@sunrise.ch>
2017-12-16 23:51:31 +0100

    minor chnages

--- src/test/java/ch/zhaw/schiffeversenken/gamestart/GameStarterTest.java
+++ src/test/java/ch/zhaw/schiffeversenken/gamestart/GameStarterTest.java
@@ -7,6 +7,7 @@ import ch.zhaw.schiffeversenken.data.Game;
 import ch.zhaw.schiffeversenken.data.PlayField;
 import ch.zhaw.schiffeversenken.data.Ship;
 import ch.zhaw.schiffeversenken.guicomponents.Maingui;
+import ch.zhaw.schiffeversenken.helpers.ComputerPlayer;
 import ch.zhaw.schiffeversenken.helpers.Coordinate;
 
 public class GameStarterTest {
@@ -15,6 +16,7 @@ public class GameStarterTest {
 		int rowCount = 20;
 		PlayField playerField = new PlayField(columnCount, rowCount);
 		PlayField computerField = new PlayField(columnCount, rowCount);
+		ComputerPlayer computerPlayer = new ComputerPlayer(columnCount, rowCount);
 		List<Coordinate> coordinates = new ArrayList<Coordinate>();
 		Coordinate coordinate = new Coordinate(0,0,false);
 		coordinates.add(coordinate);
@@ -22,7 +24,7 @@ public class GameStarterTest {
 		coordinates.add(coordinate);
 
 		computerField.addShip(new Ship(coordinates));
-		Game game = new Game(playerField, computerField);
+		Game game = new Game(playerField, computerField, computerPlayer);
 		
 		Maingui gui = new Maingui(rowCount, columnCount, game);
 		game.registerDisplay(gui);

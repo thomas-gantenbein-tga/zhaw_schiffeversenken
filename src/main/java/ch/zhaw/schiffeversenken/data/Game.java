ad293f4f0213645675a2f31d7354f1dc4dc52460
Thomas Gantenbein <tgantenbein@sunrise.ch>
2017-12-16 21:36:44 +0100

    Processes shots and could apply advanced rules (e.g. extra shots)

---
+++ src/main/java/ch/zhaw/schiffeversenken/data/Game.java
@@ -0,0 +1,36 @@
+package ch.zhaw.schiffeversenken.data;
+
+import java.util.List;
+
+import ch.zhaw.schiffeversenken.Coordinate;
+import ch.zhaw.schiffeversenken.guicomponents.Display;
+
+public class Game {
+	private PlayField playerField;
+	private PlayField computerField;
+	private List<Display> displayList;
+
+	public void processShot(PlayField playField, Coordinate shotCoordinate) {
+		playField.processShot(shotCoordinate);
+		alertDisplays();
+	}
+
+	public void registerDisplay(Display display) {
+		displayList.add(display);
+	}
+
+	private void alertDisplays() {
+		for (Display display : displayList) {
+			display.update();
+		}
+	}
+
+	public PlayField getPlayerField() {
+		return playerField;
+	}
+
+	public PlayField getComputerField() {
+		return computerField;
+	}
+
+}

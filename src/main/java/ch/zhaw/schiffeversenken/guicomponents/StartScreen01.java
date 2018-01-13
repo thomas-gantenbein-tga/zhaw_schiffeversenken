8efccf3cc9a0fd5d36aa4577f4682768cf14477f
Thomas Gantenbein <tgantenbein@sunrise.ch>
2018-01-13 11:13:35 +0100

    refined layout settings

--- src/main/java/ch/zhaw/schiffeversenken/guicomponents/StartScreen01.java
+++ src/main/java/ch/zhaw/schiffeversenken/guicomponents/StartScreen01.java
@@ -3,7 +3,12 @@ package ch.zhaw.schiffeversenken.guicomponents;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,6 +17,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
@@ -21,7 +27,6 @@ import ch.zhaw.schiffeversenken.helpers.ComputerPlayer;
 
 public class StartScreen01 {
 	private JFrame frame;
-	private ActiveGameDisplay activeGameDisplay;
 	private JTextField sizeComputerFieldInput;
 	private JTextField sizePlayerFieldInput;
 	private int sizeComputerField;
@@ -32,30 +37,57 @@ public class StartScreen01 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1600, 800);
 		Container contentPane = frame.getContentPane();
-		JPanel settingsPanel = new JPanel();
+		Insets settingsInset = new Insets(2,2,2,2);
+		
+		JLabel welcomeTextLabel = new JLabel("Enter your preferred size of your playing field and the one of your computer enemy.");
+		Font settingsFont = new Font("Sans Serif", Font.PLAIN, welcomeTextLabel.getFont().getSize());
+		welcomeTextLabel.setFont(settingsFont);
 
-		GridLayout gridLayout = new GridLayout(3, 2);
-		gridLayout.setVgap(5);
-		gridLayout.setHgap(5);
-		settingsPanel.setLayout(gridLayout);
-		contentPane.setLayout(new FlowLayout());
+		LayoutManager gridBagLayout = new GridBagLayout();
+		GridBagConstraints gbConstraints = new GridBagConstraints();
+		gbConstraints.fill = GridBagConstraints.NONE;
+		gbConstraints.anchor = GridBagConstraints.FIRST_LINE_START;
+		gbConstraints.insets = new Insets(4, 4, 4, 4);
+		contentPane.setLayout(gridBagLayout);
 
-		sizeComputerFieldInput = new JTextField();
+		sizeComputerFieldInput = new JTextField(4);
+		sizeComputerFieldInput.setMargin(settingsInset);
 		JLabel sizeComputerFieldLabel = new JLabel("Size computer field");
+		sizeComputerFieldLabel.setFont(settingsFont);
+		
+		sizePlayerFieldInput = new JTextField(4);
+		sizePlayerFieldInput.setMargin(settingsInset);
 
-		sizePlayerFieldInput = new JTextField();
 		JLabel sizePlayerFieldLabel = new JLabel("Size player field");
+		sizePlayerFieldLabel.setFont(settingsFont);
 
 		JButton startButton = new JButton("Start game");
 
-		settingsPanel.add(sizeComputerFieldLabel);
-		settingsPanel.add(sizeComputerFieldInput);
-		settingsPanel.add(sizePlayerFieldLabel);
-		settingsPanel.add(sizePlayerFieldInput);
-		settingsPanel.add(startButton);
-
-		contentPane.add(settingsPanel);
-
+		gbConstraints.gridx = 0;
+		gbConstraints.gridy = 0;
+		gbConstraints.gridwidth = 2;
+		gbConstraints.weightx = 0;
+		gbConstraints.weighty = 0;
+		contentPane.add(welcomeTextLabel, gbConstraints);
+		
+		gbConstraints.gridx = 0;
+		gbConstraints.gridy = 1;
+		gbConstraints.gridwidth = 1;
+		gbConstraints.anchor = GridBagConstraints.WEST;
+		contentPane.add(sizeComputerFieldLabel, gbConstraints);
+		gbConstraints.gridx = 1;
+		contentPane.add(sizeComputerFieldInput, gbConstraints);
+		
+		gbConstraints.gridx = 0;
+		gbConstraints.gridy = 2;
+		contentPane.add(sizePlayerFieldLabel, gbConstraints);
+		gbConstraints.gridx = 1;
+		contentPane.add(sizePlayerFieldInput, gbConstraints);
+		
+		gbConstraints.gridx = 0;
+		gbConstraints.gridy = 3;
+		contentPane.add(startButton, gbConstraints);
+		
 		startButton.addActionListener(new StartButtonListener());
 		
 		frame.setVisible(true);

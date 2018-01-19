2e6a0b39e0cb3ce8f05f05cda133a52d3efd643c
Thomas Gantenbein <tgantenbein@sunrise.ch>
2018-01-19 20:50:23 +0100

    hid the computer ships again

--- src/main/java/ch/zhaw/schiffeversenken/guicomponents/RunningGameDisplay.java
+++ src/main/java/ch/zhaw/schiffeversenken/guicomponents/RunningGameDisplay.java
@@ -214,12 +214,6 @@ public class RunningGameDisplay implements Display {
 				Shape hitShip = ShapeFactory.createShipHit(coordinate, rowCountComputer, columnCountComputer);
 				computerField.addShape(hitShip);
 			}
-			// TODO: just for testing; remove this else-Block to hide computer
-			// ships again
-			else {
-				Shape intactShip = ShapeFactory.createShipIntact(coordinate, rowCountComputer, columnCountComputer);
-				computerField.addShape(intactShip);
-			}
 		}
 
 		for (Coordinate coordinate : game.getComputerField().getFreeSea()) {
